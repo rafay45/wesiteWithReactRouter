2d6ef04d7fbe0cea0399e3e37cdd1bad07a70681
rafay45 <rafayy579@gmail.com>
2025-07-07 16:09:32 +0500

    👕🧶Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Home from './components/Home'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 
